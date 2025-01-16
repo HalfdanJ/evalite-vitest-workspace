8547f750c449d5e5d6be0a84c51d3c8f61ea284c
Jonas Jongejan <jonas@halfdanj.dk>
2025-01-16 11:04:19 -0500

    evalite sample

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest/config" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    // globals: true,
+    workspace: [
+      {
+        test: {
+          name: "unit",
+          include: ["**/*.test.ts"],
+        },
+      },
+    ],
+  },
+});
